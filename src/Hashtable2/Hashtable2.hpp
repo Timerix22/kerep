5b0098ad39a8f1fc4b5bbc07fbb60f9b0de968f7
Timerix22 <timerix11@gmail.com>
2022-06-03 20:40:41 +0300

    class header files made less weird

--- src/Hashtable2/Hashtable2.hpp
+++ src/Hashtable2/Hashtable2.hpp
@@ -3,17 +3,58 @@
 #include "../Hashtable/hash.h"
 #include "../Autoarr2/Autoarr2.hpp"
 
-template <typename TKey, typename TValue>
+template <typename TKey, typename TVal>
 class Hashtable2{
     uint8 hein;
 public:
     Autoarr2<TKey>* keys;
-    Autoarr2<TValue>* values;
+    Autoarr2<TVal>* values;
 
     Hashtable2();
-    TValue get(TKey key);
-    TValue getptr(TKey key);
-    bool addOrSet(TKey key, TValue);
+    TVal get(TKey key);
+    TVal getptr(TKey key);
+    bool addOrSet(TKey key, TVal);
     bool remove(TKey);
     virtual ~Hashtable2();
 };
+
+
+// amount of rows
+static const uint16 HT_HEIGHTS[]={17,61,257,1021,4099,16381,65521};
+#define HT_HEIN_MIN 0
+#define HT_HEIN_MAX 6
+
+#define ARR_BC 2
+#define ARR_BL 8
+
+template<typename TKey, typename TVal>
+Hashtable2<TKey, TVal>::Hashtable2() {
+    hein=HT_HEIN_MIN;
+    keys=new Autoarr2<TKey>;
+    values=new Autoarr2<TVal>;
+}
+
+template<typename TKey, typename TVal>
+TVal Hashtable2<TKey, TVal>::get(TKey key) {
+    
+}
+
+template<typename TKey, typename TVal>
+TVal Hashtable2<TKey, TVal>::getptr(TKey key) {
+
+}
+
+template<typename TKey, typename TVal>
+bool Hashtable2<TKey, TVal>::addOrSet(TKey key, TVal) {
+
+}
+
+template<typename TKey, typename TVal>
+bool Hashtable2<TKey, TVal>::remove(TKey) {
+
+}
+
+template<typename TKey, typename TVal>
+Hashtable2<TKey, TVal>::~Hashtable2() {
+
+}
