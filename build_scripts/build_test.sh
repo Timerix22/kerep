eb72368958b3f7e4eeada6c5f8a0211a2d4ca3c3
Timerix22 <timerix11@gmail.com>
2022-06-03 20:37:58 +0300

    scripts fixed again

--- build_scripts/build_test.sh
+++ build_scripts/build_test.sh
@@ -3,8 +3,8 @@
 source build_scripts/init.sh
 
 print "${CYAN}=============[build_test]=============\n"
-clear_dir bin
-clear_dir obj
+clear_dir "$OUTDIR"
+clear_dir "$OBJDIR"
 compile_c "$BUILD_TEST_C_ARGS" "$SRC_C $TESTS_C"
 compile_cpp "$BUILD_TEST_CPP_ARGS" "$SRC_CPP $TESTS_CPP"
 link "$BUILD_TEST_CPP_ARGS $BUILD_TEST_LINKER_ARGS" $TEST_FILE
