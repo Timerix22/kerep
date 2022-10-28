5e35cb6721954197beb8ffc6bdc0fceb05463e1e
timerix <timerix11@gmail.com>
2022-10-28 11:57:43 +0600

    path_concat

---
+++ src/Filesystem/filesystem.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#if __cplusplus
+extern "C" {
+#endif
+
+#include "std.h"
+
+#if defined(_WIN32) || defined (_WIN64)
+static const char path_sep='\\';
+#else
+static const char path_sep='/';
+#endif
+
+char* __path_concat(uint16 n, char* path_start, ...);
+#define path_concat(PATH_PARTS) __path_concat(count_args(PATH_PARTS), PATH_PARTS)
+
+
+
+#if __cplusplus
+}
+#endif
