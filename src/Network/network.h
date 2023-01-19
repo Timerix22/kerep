f64a8368ac159ebe2605c8d739f434279d6c2591
timerix <timerix11@gmail.com>
2023-01-20 04:26:35 +0600

    linking to winsock

--- src/Network/network.h
+++ src/Network/network.h
@@ -6,10 +6,6 @@ extern "C" {
 
 #include "../base/base.h"
 
-#if defined(_WIN64) || defined(_WIN32)
-    #define KN_USE_WINSOCK 1
-#endif
-
 #include "knAddress.h"
 #include "sockets/knSocketTCP.h"
 #include "sockets/knSocketUDP.h"
--- src/Network/stdSocketHeaders.h
+++ src/Network/stdSocketHeaders.h
@@ -4,8 +4,15 @@
 extern "C" {
 #endif
 
+#if defined(_WIN64) || defined(_WIN32)
+    #define KN_USE_WINSOCK 1
+#else 
+    #define KN_USE_WINSOCK 0
+#endif
+
 #if KN_USE_WINSOCK
     #include <winsock2.h>
+    // #include <ws2tcpip.h>
 #else
     #include <sys/types.h>
     #include <sys/socket.h>
